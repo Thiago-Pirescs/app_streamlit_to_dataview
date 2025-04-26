83d61cb8f6cb27ac580e7b435d7f6f9bfd373033
Thiago-Pirescs <thiago.pires.cs@gmail.com>
2025-04-26 19:03:19 -0300

    Adição de mais componentes para estudo e aplicação prática.

--- index.py
+++ index.py
@@ -16,4 +16,29 @@ def main():
         st.write("O usuário selecionou a opção: ", selected_option)
 
 
+    st.header("Slider")
+    slider_view = st.slider ("Escolha um valor: ", 0, 100, 50)
+    st.write("O valor escolhido é ", slider_view)
+
+    checkbox_boolean = st.checkbox ("Marque para ativar!")
+    st.write("Checkbox", checkbox_boolean)
+    
+    if st.button("Clique aqui: "):
+         st.write("Você clicou no botão!")
+
+    st.header("Loading...")
+    with st.spinner("Aguarde..."):
+        time.sleep(3)
+    st.success("Carregado!")
+
+    st.header("Upload de arquivo")
+    uploaded_file = st.file_uploader("Escolha um arquivo para subir: ", type=["pdf", "png", "jpeg"])
+
+    if uploaded_file:
+        st.write("Nome do arquivo: ", uploaded_file.name)
+
+    st.header("Grafico")
+    data = {'x': [1,2,3,4,5], 'y': [10,20,30,40,50]}
+    st.line_chart(data)         
+
 main() 
